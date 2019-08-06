cb8a1a697547279e5a4a49946c1b5247862a5707
Kelton Karboviak <kelton.karboviak@daveramsey.com>
2019-08-06 15:27:38 -0500

    Have cli call previously implemented aws_key_formatter script

--- src/aws_key_formatter/cli.py
+++ src/aws_key_formatter/cli.py
@@ -4,6 +4,7 @@ import sys
 
 import click
 
+from . import aws_key_formatter
 from .__version__ import __version__
 
 
@@ -11,14 +12,12 @@ from .__version__ import __version__
 @click.version_option(version=__version__, prog_name='aws-key-formatter')
 @click.option('-p', '--profile', default='default', show_default=True, help='AWS CLI Profile name')
 @click.option('--token/--no-token', ' -t/', default=False, show_default=True, help='Include AWS Session Token?')
-# '--profile', '-p', default='default', help='AWS CLI Profile name'
-# '--include_token', '-t', action='store_true', help='Include AWS Session Token?'
 def main(profile: str, token: bool):
     """Console script for aws_key_formatter."""
-    click.echo(
-        "Replace this message by putting your code into aws_key_formatter.cli.main"
-    )
-    click.echo("See click documentation at http://click.pocoo.org/")
+    # TODO: Have the aws_key_formatter function return the string,
+    # then have this function do the `click.echo` to output it.
+    aws_key_formatter.main(profile, token)
+
     return 0
 
 
--- tests/aws_key_formatter_test.py
+++ tests/aws_key_formatter_test.py
@@ -26,7 +26,9 @@ def test_command_line_interface():
     runner = CliRunner()
     result = runner.invoke(cli.main)
     assert result.exit_code == 0
-    assert 'aws_key_formatter.cli.main' in result.output
+    assert 'ACCESS_KEY_ID' in result.output
+    assert 'SECRET_ACCESS_KEY' in result.output
+
     help_result = runner.invoke(cli.main, ['--help'])
     assert help_result.exit_code == 0
-    assert 'Console script for aws_key_formatter.' in help_result.output
+    assert 'aws_key_formatter' in help_result.output
